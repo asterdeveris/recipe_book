19baedb5aa3d6c7c16c23aa1880c5345a5386d03
Dasha <asterdeveris@gmail.com>
2023-02-15 12:32:16 +0200

    add editing recipe, try to change recipe in array

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-const addButton = document.querySelector("#add-recipe");
-const form = document.querySelector(".form");
 const listOfRecepies = document.querySelector(".list-of-recepies");
+
+// Inside form variables
+const form = document.querySelector(".form");
+const recipeNameForm = document.getElementById("recipe-name");
+const ingridientsForm = document.getElementById("ingridients");
+const directionsForm = document.getElementById("directions");
 const closeButton = document.querySelector("#close");
+const submitButton = document.querySelector("[type='submit']");
+
+// Find buttons for editing list and recepies
+const addButton = document.querySelector("#add-recipe");
+const editButton = document.querySelector("#edit");
+
+// Chosen recipe
+const recipeHeader = document.querySelector(".recipe-header-h");
 const ingridientsList = document.querySelector(".recipe-ingridients-list");
 const directionsList = document.querySelector(".recipe-directions-list");
-const recipeHeader = document.querySelector(".recipe-header-h");
 
 const recipeStore = JSON.parse(localStorage.getItem("recipeStore")) || [];
 
-function openForm() {
-  form.classList.remove("hide");
+function openForm(e) {
+  if (e.target.parentElement === addButton) {
+    submitButton.value = `Add`;
+    form.classList.remove("hide");
+  } else if (e.target.parentElement === editButton) {
+    openEditingRecipe();
+  }
 }
 
 function addRecipe(e) {
   e.preventDefault();
-  const name = document.getElementById("recipe-name").value;
-  const ingridients = document.getElementById("ingridients").value.split("\n");
-  const directions = document.getElementById("directions").value.split("\n");
+  const name = recipeNameForm.value;
+  const ingridients = ingridientsForm.value.split("\n");
+  const directions = directionsForm.value.split("\n");
 
   const recipe = {
     name,
@@ -40,14 +56,15 @@ function populateListOfRecepies(recepies, recepiesList) {
     .join("");
 }
 
-function closeModal() {
+function closeModal(e) {
   form.classList.add("hide");
+  form.reset();
 }
 
-function showRecipe() {
-  const { name, ingridients, directions } = recipeStore.find(
-    (el) => el.name === e.target.innerHTML
-  );
+function showRecipe(recipeName) {
+  const recipe = recipeStore.find((el) => el.name === recipeName);
+
+  const { name, ingridients, directions } = recipe;
 
   recipeHeader.innerHTML = name;
 
@@ -64,8 +81,54 @@ function showRecipe() {
     .join("");
 }
 
+function openEditingRecipe(e) {
+  const editingRecipe = recipeStore.find(
+    (el) => el.name === recipeHeader.innerHTML
+  );
+  const inx = recipeStore.findIndex((el) => el.name === recipeHeader.innerHTML);
+
+  const { ingridients, directions } = editingRecipe;
+
+  ingridientsForm.value = ingridients
+    .map((ingridient) => {
+      return `${ingridient}\n`;
+    })
+    .join("");
+
+  recipeNameForm.value = recipeHeader.innerHTML;
+
+  directionsForm.value = directions
+    .map((direction) => {
+      return `${direction}\n`;
+    })
+    .join("");
+
+  submitButton.value = `Save`;
+  form.classList.remove("hide");
+
+  submitButton.addEventListener("click", () => {
+    if ((submitButton.value = "Save")) {
+      recipeStore[inx].name = recipeNameForm.value;
+      recipeStore[inx].ingridients = ingridientsForm.value.trim().split("\n");
+      recipeStore[inx].directions = directionsForm.value.trim().split("\n");
+      console.log(recipeStore[inx]);
+
+      showRecipe(recipeStore[inx].name);
+    }
+
+    populateListOfRecepies(recipeStore, listOfRecepies);
+  });
+}
+
 addButton.addEventListener("click", openForm);
-form.addEventListener("submit", addRecipe);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (submitButton.value === "Add") {
+    addRecipe(e);
+  }
+});
+
 closeButton.addEventListener("click", closeModal);
-listOfRecepies.addEventListener("click", showRecipe);
+listOfRecepies.addEventListener("click", (e) => showRecipe(e.target.innerHTML));
+editButton.addEventListener("click", openForm);
 populateListOfRecepies(recipeStore, listOfRecepies);
